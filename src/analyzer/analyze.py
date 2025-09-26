3dc94a8ae717f0292fdfadf51df813db97f92815
Mafusn <Magnus.peetz.holt@gmail.com>
2025-09-26 14:04:21 +0200

    Update analyze.py

--- src/analyzer/analyze.py
+++ src/analyzer/analyze.py
@@ -11,13 +11,12 @@ def find_letter_candidates():
     cipher = load_cipher(EXAMPLE_CIPHERS_PATH / "cipher.json")
     cipher_frequencies = get_cipher_frequencies(cipher)
     letter_frequencies = load_letter_frequencies("english")
+    print(f"Loaded cipher with {(cipher_frequencies)} unique symbols")
 
     # Go through each letter in letter frequencies starting from the least frequent
     sorted_letters = sorted(letter_frequencies.items(), key=lambda item: item[1])
     
     for letter, freq in sorted_letters:
-        if letter not in 'A':
-            continue
         print(f"Analyzing letter: {letter} with frequency {freq}")
         
         min_max_range = calculate_target_range(len(cipher_frequencies), freq)
@@ -30,12 +29,12 @@ def find_letter_candidates():
         
         # Find all subsets of cipher symbols whose frequencies sum to the target range
         candidates = []
-        for i in range(9, 11):
+        for i in range(min_max_range[0], min_max_range[1] + 1):
             print(f"  Finding candidates with {i} homophones and frequency {freq}")
-            # candidates += backtracking(
-            #     cipher_frequencies,
-            #     i,
-            #     freq
-            # )
+            candidates += backtracking(
+                cipher_frequencies,
+                i,
+                freq
+            )
         print(f"  Found {len(candidates)} candidate sets for letter '{letter}'")
         
